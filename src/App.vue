6cf6f3f6445725f52d46be1cdd92b6dbd49c83e9
Zixuip <854066271@qq.com>
2020-05-17 16:02:41 +0800

    左侧导航布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <leftNav></leftNav>
+    <router-view />
   </div>
 </template>
 
 <script>
+import leftNav from "@/components/common/leftNav";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    leftNav
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main {
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="left_nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-goumai"></i>
+        <div>收银</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-dianpu"></i>
+        <div>店铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-hanbao"></i>
+        <div>商品</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-huiyuanqia"></i>
+        <div>会员</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-quanbudingdan"></i>
+        <div>统计</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-gongnengjianyi"></i>
+        <div>设置</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.left_nav {
+  color: aliceblue;
+  font-size: 10px;
+  height: 100%;
+  background-color: #1d8ce0;
+  float: left;
+  width: 5%;
+}
+.iconfont {
+  font-size: 24px;
+}
+.left_nav ul {
+  padding: 0;
+  margin: 0;
+}
+.left_nav li {
+  list-style: none;
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding: 10px;
+}
+</style>
